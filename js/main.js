a06ebd7acfab192ba4130037d694a63eaf7f7e37
ngf93 <nabiullinaguzel93@gmail.com>
2022-11-19 20:22:52 +0300

    table js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
+/* show/hide sublevel elements */
+function toggleList(btn) {
+  elems = btn.id;
+  let arr = Array.from(document.getElementsByClassName(elems));
+  if (btn.classList.contains('more')) {
+    arr.forEach(item => {
+      item.style.display = 'grid'
+    })
+    btn.classList.remove('more')
+  } else {
+    arr.forEach(item => {
+      item.style.display = 'none'
+    })
+    btn.classList.add('more')
+  }
+}
+
+const hoverDivs = document.getElementsByClassName('hover-effect')
+if (hoverDivs){
+  const Arr = Array.from(hoverDivs)
+  Arr.forEach(item => {
+    item.addEventListener('mouseenter', (event) => {
+      console.log('mouseenter')
+      const parent = item.parentElement
+      const parentChildren = Array.from(parent.children);
+      index = parentChildren.indexOf(item)
+      console.log(index)
+
+      const allRows = Array.from(document.getElementsByClassName('table-grid-row'))
+
+      allRows.forEach(item => {
+        item.children[index].classList.add('hover-column')
+      })
+    })
+    item.addEventListener('mouseleave', (event) => {
+      console.log('mouseleave')
+      const parent = item.parentElement
+      const parentChildren = Array.from(parent.children);
+      index = parentChildren.indexOf(item)
+      console.log(index)
+
+      const allRows = Array.from(document.getElementsByClassName('table-grid-row'))
+
+      allRows.forEach(item => {
+        item.children[index].classList.remove('hover-column')
+      })
+    })
+  })
+}
+
+
 const swiper = new Swiper('.swiper-1', {
     loop: true,
     effect: 'fade',
@@ -11,4 +62,4 @@ const swiper = new Swiper('.swiper-1', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
+});
