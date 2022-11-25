d67211fdc72388adfc7e9b6aaf8bb26f544a6229
ngf93 <nabiullinaguzel93@gmail.com>
2022-11-25 15:19:42 +0300

    main page desktop

--- js/sliders.js
+++ js/sliders.js
@@ -12,3 +12,19 @@ const swiper = new Swiper('.swiper-1', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+const swiper2= new Swiper('.swiper-brands', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  }
+});
