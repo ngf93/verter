b856a9491acf04b1bd8f4470c3dfb37ce8e6883c
ngf93 <nabiullinaguzel93@gmail.com>
2022-12-26 08:19:50 +0300

    functions tooltip

--- js/main.js
+++ js/main.js
@@ -155,3 +155,8 @@ window.onload = function() {
     })
   }
 }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
